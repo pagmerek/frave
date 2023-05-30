7f2960ebc7f2912b0d3dd4c765abb697b79182bc
pagmerek <p.gmerek7@gmail.com>
2023-05-30 17:01:08 +0200

    Code refactor

--- src/commands.rs
+++ src/commands.rs
@@ -3,38 +3,31 @@ use std::path::PathBuf;
 
 use crate::decoder::Decoder;
 use crate::encoder::Encoder;
-use crate::frave_image::FraveImage;
-use crate::variants::{get_variant, Variant};
+use crate::frave_image::{get_quantization_matrix, FraveImage, Frv};
+use crate::variants::Variant;
 
 pub fn encode(path: PathBuf, var: Variant, output: String) {
     let img = image::open(path).unwrap_or_else(|e| {
         panic!("Failed to open: {}", e);
     });
 
-    let mut enc = Encoder::new(img.into_luma8(), get_variant(var));
+    let mut enc = FraveImage::new_from_img(img.into_luma8(), var);
     enc.find_coef();
-    enc.quantizate();
-    let (compressed, contexts) = enc.ans_encode();
-    let frave_image = FraveImage {
-        height: enc.height,
-        width: enc.width,
-        depth: enc.depth,
-        center: (enc.center.x, enc.center.y),
-        variant: var,
-        ans_contexts: contexts,
-        compressed_coef: compressed,
-    };
+    enc.quantizate(&get_quantization_matrix());
+    let (coef, contexts) = enc.ans_encode();
+    let compressed = Frv::new(enc.height, enc.width, contexts, var, coef);
     let mut f = File::create(output).unwrap();
-    bincode::serialize_into(&mut f, &frave_image).unwrap();
+    bincode::serialize_into(&mut f, &compressed).unwrap();
 }
 
 pub fn decode(path: PathBuf, output: String) {
     let img = File::open(path).unwrap_or_else(|e| {
         panic!("Failed to open: {}", e);
     });
-    let frv_img: FraveImage = bincode::deserialize_from(img).unwrap();
-    let mut dec = Decoder::new(frv_img);
-    dec.unquantizate();
+    let frv_img: Frv = bincode::deserialize_from(img).unwrap();
+    let mut dec = FraveImage::new_from_frv(&frv_img);
+    dec.ans_decode(frv_img.compressed_coef, frv_img.ans_contexts);
+    dec.unquantizate(&get_quantization_matrix());
     dec.find_val();
     dec.image.save(output).unwrap_or_else(|e| {
         panic!("Failed to save: {}", e);

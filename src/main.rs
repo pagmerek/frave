1a85017b67cc4ac3ce6b6c438e96244fd0f702ac
pagmerek <p.gmerek7@gmail.com>
2023-05-30 17:06:40 +0200

    Move cli to main file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
-use clap::Parser;
-
-use frave::cli::{Cli, Commands};
 use frave::commands::{decode, encode};
+use std::path::PathBuf;
+
+use frave::variants::Variant;
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+/// Image compression program based on complex based numeral systems
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Decode(Decode),
+    Encode(Encode),
+}
+
+#[derive(Args)]
+/// Encodes bitmap file to frave format
+pub struct Encode {
+    pub bmp_path: PathBuf,
+
+    #[arg(short, long, value_enum, default_value_t = Variant::TameTwindragon)]
+    pub variant: Variant,
+
+    #[arg(short, default_value_t = String::from("a.frv"))]
+    pub output: String,
+}
+
+#[derive(Args)]
+/// Decodes frave file to bitmap format
+pub struct Decode {
+    pub fr_path: PathBuf,
+
+    #[arg(short, default_value_t = String::from("a.bmp"))]
+    pub output: String,
+}
 
 fn main() {
     let cli = Cli::parse();
